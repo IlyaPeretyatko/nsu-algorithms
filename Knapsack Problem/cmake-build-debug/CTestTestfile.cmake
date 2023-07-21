b1c3fbe28709082b8fee8b4d9e250364c84717e1
i.peretyatko <i.peretyatko@g.nsu.ru>
2023-07-21 20:42:29 +0700

    fix

--- Knapsack Problem/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/nsu_repos/template/lab11
-# Build directory: D:/nsu_repos/template/lab11/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(testlab11.exe "D:/nsu_repos/template/lab11/test/testlab11.exe" "./lab11")
-set_tests_properties(testlab11.exe PROPERTIES  DEPENDS "lab11" _BACKTRACE_TRIPLES "D:/nsu_repos/template/modules/test_runner.cmake;23;add_test;D:/nsu_repos/template/modules/test_runner.cmake;0;;D:/nsu_repos/template/modules/common_lab.cmake;3;include;D:/nsu_repos/template/modules/common_lab.cmake;0;;D:/nsu_repos/template/lab11/CMakeLists.txt;9;include;D:/nsu_repos/template/lab11/CMakeLists.txt;0;")
--- Topological Sort/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/nsu_repos/template/lab7
-# Build directory: D:/nsu_repos/template/lab7/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(testlab7.exe "D:/nsu_repos/template/lab7/test/testlab7.exe" "./lab7")
-set_tests_properties(testlab7.exe PROPERTIES  DEPENDS "lab7" _BACKTRACE_TRIPLES "D:/nsu_repos/template/modules/test_runner.cmake;23;add_test;D:/nsu_repos/template/modules/test_runner.cmake;0;;D:/nsu_repos/template/modules/common_lab.cmake;3;include;D:/nsu_repos/template/modules/common_lab.cmake;0;;D:/nsu_repos/template/lab7/CMakeLists.txt;9;include;D:/nsu_repos/template/lab7/CMakeLists.txt;0;")
